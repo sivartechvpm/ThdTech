103c6099f0c00a708568159987367f17b9eb202c
sivartechvpm <sivartechvpm@gmail.com>
2023-06-21 12:23:43 +0530

    code commit

--- ThdTech/src/learn/java/GetInput.java
+++ ThdTech/src/learn/java/GetInput.java
@@ -1,9 +1,32 @@
 package learn.java;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class GetInput {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+		
+		// Buffered Reader 
+	/*	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine());*/
+		
+		// Scanner 
+		/*Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		System.out.println(input);*/
+		
+	
+		//System Console
+		//String input = System.console().readLine();
+		//System.out.println(input);
+		
+		
+		//System.out.println(args[0]);
+		
 		
 		
 	}
